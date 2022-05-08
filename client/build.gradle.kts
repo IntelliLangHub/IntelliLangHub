bde33993149455d597d66c53b341382ffbebe922
Ryazanov Daniil <rybbba@gmail.com>
2022-05-08 22:38:29 +0300

    Add intelliLang integration

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.jetbrains.intellij") version "1.3.1"
-    kotlin("jvm") version "1.5.30"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
     java
 }
 
@@ -13,6 +14,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
@@ -20,6 +22,7 @@ dependencies {
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
     version.set("2021.3.1")
+    plugins.add("org.intellij.intelliLang")
 }
 tasks {
     patchPluginXml {
--- client/src/main/kotlin/org/example/langClient/ruleActions/PushAction.kt
+++ client/src/main/kotlin/org/example/langClient/ruleActions/PushAction.kt
@@ -5,12 +5,21 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
+@Suppress("PROVIDED_RUNTIME_TOO_LOW")  // https://github.com/Kotlin/kotlinx.serialization/issues/993
+@Serializable
+data class PushedInjection(
+    val name: String,
+    val rule: String?,
+    val library: String,
+)
 
 class PushAction : AnAction() {
     override fun update(e: AnActionEvent) {
@@ -24,21 +33,23 @@ class PushAction : AnAction() {
         val project: Project? = e.project
         val editor = e.getData(CommonDataKeys.EDITOR)
 
-        if (editor?.selectionModel?.selectedText == null) {
+        val serverUrl = "http://localhost:8090"
+
+        val rule = editor?.selectionModel?.selectedText
+        if (rule == null) {
             Messages.showMessageDialog(project, "No text was selected.", "Error", Messages.getInformationIcon())
             return
         }
-        val name = editor.selectionModel.selectedText
-        val requestJson = "{ \"name\":\"$name\", \"rule\":\"$name\", \"library\":\"test\" }"
+        val injection = PushedInjection("Test Rule", rule, "testlib")
 
         val request : HttpRequest = HttpRequest.newBuilder()
-            .uri(URI.create("http://localhost:8090"))
+            .uri(URI.create(serverUrl))
             .header("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString(requestJson))
+            .POST(HttpRequest.BodyPublishers.ofString(Json.encodeToString(injection)))
             .build()
 
         val client: HttpClient = HttpClient.newBuilder().build()
-        val response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
 
         Messages.showMessageDialog(project, response.body(), "Created Rule", Messages.getInformationIcon())
     }
