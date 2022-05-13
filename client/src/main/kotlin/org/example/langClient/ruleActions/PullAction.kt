da8520fe3a3b2bf318510f846b2ad451e95a7bff
Ryazanov Daniil <rybbba@gmail.com>
2022-05-13 18:36:14 +0300

    Pull rules for project libraries

--- client/src/main/kotlin/org/example/langClient/ruleActions/PullAction.kt
+++ client/src/main/kotlin/org/example/langClient/ruleActions/PullAction.kt
@@ -3,6 +3,7 @@ package org.example.langClient.ruleActions
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.libraries.*
 import com.intellij.openapi.ui.Messages
 import org.intellij.plugins.intelliLang.Configuration
 import org.intellij.plugins.intelliLang.inject.InjectorUtils
@@ -25,33 +26,47 @@ data class PulledInjectionPack(
     val id: String
 )
 
+val libNameRegex = Regex("^.+?:\\s*(.+)\\s*:.*\$") // Probably needs refactoring
+
 class PullAction : AnAction() {
     override fun update(e: AnActionEvent) {
-        super.update(e)
+        val project: Project? = e.project
+
+        e.presentation.isEnabledAndVisible = (project != null)
     }
 
     override fun actionPerformed(e: AnActionEvent) {
-        val project: Project? = e.project
+        val project: Project = e.project ?: return
 
         val serverUrl = "http://localhost:8090"
-        val lib = "testlib"
 
-        val request : HttpRequest = HttpRequest.newBuilder()
-            .uri(URI.create("$serverUrl/$lib"))
-            .build()
+        val libTable = LibraryTablesRegistrar.getInstance()
+        val libs : MutableList<Library> = ArrayList()
+        //libs.addAll(libTable.libraryTable.libraries)
+        libs.addAll(libTable.getLibraryTable(project).libraries)
 
-        val client: HttpClient = HttpClient.newBuilder().build()
-        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+        val packs : MutableList<PulledInjectionPack> = ArrayList()
+        for (lib in libs) {
+            val matchResult = libNameRegex.matchEntire(lib.name ?: continue)
+            val nameStripped = matchResult?.groupValues?.get(1) ?: continue
 
-        if (response.statusCode() != 200) {
-            Messages.showWarningDialog(project,
-                "Cannot get rules", "Request Error")
-            return
+            try {
+                val request: HttpRequest = HttpRequest.newBuilder()
+                    .uri(URI.create("$serverUrl/$nameStripped"))
+                    .build()
+
+                val client: HttpClient = HttpClient.newBuilder().build()
+                val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+                packs.addAll(Json.decodeFromString<List<PulledInjectionPack>>(response.body()))
+            } catch (ex: Exception) {
+                Messages.showWarningDialog(project,
+                    "Cannot get rules", "Request Error")
+                return
+            }
         }
 
         var hasImportErrors = false
 
-        val packs = Json.decodeFromString<List<PulledInjectionPack>>(response.body())
         for (pack in packs) {
             try {
                 val cfgStream = pack.rule.byteInputStream()
