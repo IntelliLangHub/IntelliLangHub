27403ba4a00b53ec6326207bb6602211f0d5a08f
Daniil Ryazanov <rybbba@gmail.com>
2022-05-13 22:03:24 +0300

    Pull rules for client libraries (#3)

--- client/src/main/kotlin/org/example/langClient/ruleActions/PullAction.kt
+++ client/src/main/kotlin/org/example/langClient/ruleActions/PullAction.kt
@@ -3,6 +3,7 @@ package org.example.langClient.ruleActions
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.libraries.*
 import com.intellij.openapi.ui.Messages
 import org.intellij.plugins.intelliLang.Configuration
 import org.intellij.plugins.intelliLang.inject.InjectorUtils
@@ -15,53 +16,61 @@ import java.net.http.HttpResponse
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
 
-@Suppress("PROVIDED_RUNTIME_TOO_LOW")  // https://github.com/Kotlin/kotlinx.serialization/issues/993
-@Serializable
-data class PulledInjectionPack(
-    val name: String,
-    val rule: String,
-    val library: String,
-    val createdDate: String,
-    val id: String
-)
+val libNameRegex = Regex("^.+?:\\s*(.+)\\s*:.*\$") // Probably needs refactoring
 
 class PullAction : AnAction() {
     override fun update(e: AnActionEvent) {
-        super.update(e)
+        val project: Project? = e.project
+
+        e.presentation.isEnabledAndVisible = (project != null)
     }
 
     override fun actionPerformed(e: AnActionEvent) {
-        val project: Project? = e.project
+        val project: Project = e.project ?: return
 
         val serverUrl = "http://localhost:8090"
-        val lib = "testlib"
 
-        val request : HttpRequest = HttpRequest.newBuilder()
-            .uri(URI.create("$serverUrl/$lib"))
-            .build()
+        val libTable = LibraryTablesRegistrar.getInstance()
+        val libs: MutableList<Library> = ArrayList()
+        //libs.addAll(libTable.libraryTable.libraries)
+        libs.addAll(libTable.getLibraryTable(project).libraries)
 
-        val client: HttpClient = HttpClient.newBuilder().build()
-        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+        val rules: MutableList<String> = ArrayList()
+        for (lib in libs) {
+            val matchResult = libNameRegex.matchEntire(lib.name ?: continue)
+            val nameStripped = matchResult?.groupValues?.get(1) ?: continue
 
-        if (response.statusCode() != 200) {
-            Messages.showWarningDialog(project,
-                "Cannot get rules", "Request Error")
-            return
+            try {
+                val request: HttpRequest = HttpRequest.newBuilder()
+                    .uri(URI.create("$serverUrl/injection-pack/$nameStripped"))
+                    .build()
+
+                val client: HttpClient = HttpClient.newBuilder().build()
+                val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+                if (response.statusCode() == 404) continue
+
+                rules.addAll(Json.decodeFromString<List<String>>(response.body()))
+            } catch (ex: Exception) {
+                Messages.showWarningDialog(
+                    project,
+                    "Cannot get rules", "Request Error"
+                )
+                return
+            }
         }
 
         var hasImportErrors = false
 
-        val packs = Json.decodeFromString<List<PulledInjectionPack>>(response.body())
-        for (pack in packs) {
+        for (rule in rules) {
             try {
-                val cfgStream = pack.rule.byteInputStream()
+                val cfgStream = rule.byteInputStream()
                 val cfg = Configuration.load(cfgStream) ?: continue
 
                 val injections: MutableList<BaseInjection> = ArrayList()
                 for (supportId in InjectorUtils.getActiveInjectionSupportIds()) {
                     injections.addAll(cfg.getInjections(supportId))
                 }
-                Configuration.getProjectInstance(project).replaceInjections(injections, emptyList(), false)
+                Configuration.getProjectInstance(project).replaceInjections(injections, injections, false)
             } catch (ex: Exception) {
                 hasImportErrors = true
             }
@@ -73,8 +82,10 @@ class PullAction : AnAction() {
                 "Rules have been imported", "Import Successful", Messages.getInformationIcon()
             )
         } else {
-            Messages.showWarningDialog(project,
-                "Errors occurred, not all rules were imported", "Import Errors")
+            Messages.showWarningDialog(
+                project,
+                "Errors occurred, not all rules were imported", "Import Errors"
+            )
         }
     }
 }
